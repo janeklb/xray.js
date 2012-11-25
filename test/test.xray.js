ae1b356a6292488b748ab5ffd37f21ce900f734b
Janek Lasocki-Biczysko <iamjanek@gmail.com>
2012-11-25 18:15:31 +0000

    added more tests

--- test/test.xray.js
+++ test/test.xray.js
@@ -12,15 +12,47 @@ var testObject = {
 
 };
 
-describe("xray", function() {
+describe("xray.js", function() {
 
-    it("should be a function", function() {
+    it("should be a function available in the global/window scope", function() {
         expect(window.xray).to.be.a("function");
     });
 
-    it("should respect max depth", function() {
-        var paths = xray(testObject, "valA", { max_depth: 1});
-        expect(paths.length).to.be(1);
-        expect(paths[0]).to.be('o.propA');
+    describe("max depth option", function() {
+        it("should be off by default", function() {
+            var paths = xray(testObject, "valA");
+            expect(paths.length).to.be(2);
+            expect(paths[0]).to.be('o.propA');
+            expect(paths[1]).to.be('o.arrayA[4].nestedObjectProp');
+        });
+        it("should be off if zero is specified as value", function() {
+            var paths = xray(testObject, "valA", { max_depth: 0 });
+            expect(paths.length).to.be(2);
+            expect(paths[0]).to.be('o.propA');
+            expect(paths[1]).to.be('o.arrayA[4].nestedObjectProp');
+        });
+        it("should respect a non-zero value", function() {
+            var paths = xray(testObject, "valA", { max_depth: 1});
+            expect(paths.length).to.be(1);
+            expect(paths[0]).to.be('o.propA');
+        });
+    });
+
+    describe("scan keys option", function() {
+        it("shouldn't scan keys by default", function() {
+            var paths = xray(testObject, "propA");
+            expect(paths.length).to.be(0);
+        });
+        it("should scan keys if enabled", function() {
+            var paths = xray(testObject, "propA", { scan_keys: true });
+            expect(paths.length).to.be(1);
+            expect(paths[0]).to.be("o.propA");
+        });
+    });
+
+    describe("query parameter", function() {
+        it("should accept a regular string");
+        it("should accept a RegExp object");
+        it("should accept a function");
     });
 });
